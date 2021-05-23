0605a2f2c7d4390a1fb5322186f2590bed4274d2
bachir <mohamadbachirkhoder01@gmail.com>
2021-05-23 14:10:32 +0300

    step 8_a

---
+++ basic01/step8.php
@@ -0,0 +1,8 @@
+<?php
+
+$food=array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon");
+
+echo "Hello Sir, do you have  ".$food[0]."  ".$food[1]." and ".$food[2]."?  Also if you sell fruits can I find a ".$food[5]." ? ";
+
+
+?>
