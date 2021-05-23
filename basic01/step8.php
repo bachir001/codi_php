c9f4830f26ea07b114d9cbbed6d3d5c33a15eaa9
bachir <mohamadbachirkhoder01@gmail.com>
2021-05-23 14:30:56 +0300

    step 8_b

--- basic01/step8.php
+++ basic01/step8.php
@@ -1,8 +1,12 @@
 <?php
 
-$food=array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon");
+$food=array($Eggs= array( 'balade', 'mazere3' ), $milk=array ('Fresh', 'Taanayel'), "Cheese",   $WaterPack=array  ( 'Tanoureen', 'Reem'), "Tissues", "Watermelon");
 
 echo "Hello Sir, do you have  ".$food[0]."  ".$food[1]." and ".$food[2]."?  Also if you sell fruits can I find a ".$food[5]." ? ";
 
+echo"<br>";
+echo"<br>";
+
+echo "Hey Sir, Please I need 1 pack of  ".$food[0][0]." eggs and 3  ".$food[3][1];
 
 ?>
